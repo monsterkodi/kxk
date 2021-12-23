e9bd68212b45cd6286fde05e132a9e41e554673f
monsterkodi <monsterkodi@gmx.net>
2021-12-23 20:20:19 +0100

    args

--- js/args.js
+++ js/args.js
@@ -1,6 +1,6 @@
-// monsterkodi/kode 0.200.0
+// monsterkodi/kode 0.201.0
 
-var _k_
+var _k_ = {empty: function (l) {return l==='' || l===null || l===undefined || l!==l || typeof(l) === 'object' && Object.keys(l).length === 0}, valid: undefined}
 
 var args, karg, kerror, kxk, post, slash
 
@@ -21,11 +21,28 @@ else
     slash = kxk.slash
 
     args = {}
-    args.init = function (cfg)
+    args.init = function (cfg, pkg)
     {
-        var k, v
+        var k, pkgDir, pkgJson, v
 
+        if (!(pkg != null))
+        {
+            pkgDir = slash.pkg(__dirname)
+            while (!_k_.empty((pkgDir)) && slash.file(slash.dir(pkgDir)) === 'node_modules')
+            {
+                pkgDir = slash.pkg(slash.dir(pkgDir))
+            }
+            if (!_k_.empty(pkgDir))
+            {
+                pkgJson = slash.join(pkgDir,'package.json')
+                pkg = require(pkgJson)
+            }
+        }
         delete args.init
+        if (!_k_.empty(pkg))
+        {
+            cfg += `\nversion   ${pkg.version}`
+        }
         for (k in karg(cfg))
         {
             v = karg(cfg)[k]
